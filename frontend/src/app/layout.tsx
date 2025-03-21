1a5e5f98211507d6b2c0c00edd01eaa3ce23f464
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-03-21 18:43:17 +0530

    added icon and backend url

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,7 +15,8 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "chat.io",
   description: "A simple chat app",
-  authors:[{name: "devShadow"}]
+  authors:[{name: "devShadow"}],
+  icons:{icon : "/icon.png"}
 };
 
 export default function RootLayout({
