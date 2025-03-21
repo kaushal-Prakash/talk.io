23557753c9d8425adf5fb474db83f308095e9015
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-03-21 18:50:51 +0530

    removed mongo support

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,14 @@ import express from "express";
 import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
-import messageRoutes from "./routes/messageRoutes.js";
-import Message from "./models/message.js";
 import http from "http";
-import connectDB from "./utils/connectDB.js";
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
@@ -20,9 +17,6 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-app.use("/msg", messageRoutes);
-
-// Socket.IO
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
@@ -44,8 +38,6 @@ io.on("connection", (socket) => {
     console.log("A user disconnected:", socket.id);
   });
 });
-
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
--- backend/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getMessages, sendMessage } from "../contrllers/messageController.js";
-
-const router = express.Router();
-
-router.post("/messages", sendMessage);
-router.get("/messages/:room", getMessages);
-
-export default router;
