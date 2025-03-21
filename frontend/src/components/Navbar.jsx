bdc918537808568c52c893a4c6831ccbbae2d828
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-03-21 19:54:43 +0530

    removed home icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,14 +17,6 @@ function Navbar() {
           </h1>
         </div>
 
-        {/* Home Link Button */}
-        <Link
-          href="/"
-          className="flex items-center bg-blue-600 hover:bg-gradient-to-br from-purple-600 to-blue-400 text-white px-4 py-2 rounded-lg transition-all duration-300 font-semibold"
-        >
-          <FaHome className="mr-2" />
-          HOME
-        </Link>
       </div>
     </nav>
   );
